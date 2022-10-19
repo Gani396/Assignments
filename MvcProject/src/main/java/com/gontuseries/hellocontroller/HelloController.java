46ee1c990072a88dfadde1f0afe8bd6840f56478
Gani396 <60483282+Gani396@users.noreply.github.com>
2022-10-19 11:27:11 +0530

    Add files via upload

---
+++ MvcProject/src/main/java/com/gontuseries/hellocontroller/HelloController.java
@@ -0,0 +1,23 @@
+package com.gontuseries.hellocontroller;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+
+public class HelloController extends AbstractController{
+	
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest request,
+			HttpServletResponse reponse) throws Exception{
+		
+		ModelAndView modelandview = new ModelAndView("HelloPage");
+		modelandview.addObject("WelcomeMessage", "HelloWorld");
+		
+		return modelandview;
+	}
+	
+	
+
+}
---
+++ MvcProject/src/main/webapp/WEB-INF/HelloPage.jsp
@@ -0,0 +1,8 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+	<h1>First Spring MVC Application</h1>
+	<h2>${WelcomeMessage}</h2>
+</body>
+</html>
